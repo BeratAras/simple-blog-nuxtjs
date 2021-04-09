d051f36924c0899b3156143474e9233bda0e456a
Berat Aras <berat@webeyondcreative.com>
2021-04-09 18:22:06 +0300

    init

---
+++ components/common/Banner.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container-fluid bg p-0 d-flex justify-content-center align-items-center">
+    <div class="overlay"></div>
+    <h1 class="text-center text-white">Nuxt.js | Köse Yazıları</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Banner'
+}
+</script>
+
+<style scoped>
+
+</style>
